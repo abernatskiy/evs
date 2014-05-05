4e8bf023ccedf609a947006e4dbde77979d6b317
Anton Bernatskiy <abernats@uvm.edu>
2014-05-05 00:21:20 -0500

    Test client for floatVector/unixPipe added
    	new file:   tests/testFloatVectorWUnixPipe.py

---
+++ tests/testFloatVectorWUnixPipe.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2
+
+import numpy as np
+
+# compute sums of values written into the ouput pipe of the evolution,
+# scale it to fit into [0,1] and write the result into the input pipe 
+# of the evolution
+
+fnindivs = '/tmp/individuals.pipe'
+fnevals = '/tmp/evaluations.pipe'
+
+while True:
+	indivs = np.loadtxt(fnindivs)
+	ids = np.array(indivs[:,0], dtype=np.int64)
+	rawEvals = np.sum(indivs[:,1:], 1)
+	maxsc = max(rawEvals)
+	minsc = min(rawEvals)
+	normalize = np.vectorize(lambda x: (x - minsc)/(maxsc - minsc))
+	evals = normalize(rawEvals)
+	fevals = open(fnevals, 'w')
+	for i in xrange(len(ids)):
+		fevals.write(str(ids[i]) + ' ' + str(evals[i]) + '\n')
+	fevals.close()
