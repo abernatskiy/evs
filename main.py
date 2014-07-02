1ad2668cb1a835d719a59a7a4cb5dedb3ce858a9
Anton Bernatskiy <abernats@uvm.edu>
2014-07-02 20:45:03 +1000

    Minor changes
    main.py modified, small bug in logging is fixed, feature request
    is added to TODO.
    
    This commit is intended to save the state of the program during
    a particular evolutionary experiment (see https://docs.google.com/
    document/d/1MLPGYKzeDlH042ZWT6AX8W2v78-eZMMQvpmCfxUzMvg/edit?usp=sharing).

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python2
 
-# from individuals.trinaryVector import Individual
-from individuals.floatVector import Individual
+from individuals.trinaryVector import Individual
+#from individuals.floatVector import Individual
 
-# from communicators.unixPipe import Communicator
-from communicators.textFile import Communicator
+from communicators.unixPipe import Communicator
+#from communicators.textFile import Communicator
 
-# from evolvers.hillClimber import Evolver
+#from evolvers.hillClimber import Evolver
 from evolvers.afpo import Evolver
+#from evolvers.averagingAfpo import Evolver
 
-indivParams = {'length': 12, 'precision': 4, 'mutationProbability': 0.03, 'mutationAmplitude': 0.1}
-evolParams = {'indivClass': Individual, 'populationSize': 15}
+indivParams = {'length': 8, 'precision': 4, 'mutationProbability': 0.03, 'mutationAmplitude': 0.1}
+evolParams = {'indivClass': Individual, 'populationSize': 30}
 
-# comm = Communicator('evaluations.pipe', 'individuals.pipe')
-comm = Communicator()
+comm = Communicator('evaluations.pipe', 'individuals.pipe')
+#comm = Communicator('evaluations.txt', 'individuals.txt')
 
 evolver = Evolver(comm, indivParams, evolParams)
 
