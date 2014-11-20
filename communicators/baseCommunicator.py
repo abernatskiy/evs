679052387b2af9d51b0955e1e11f12d13bfc1da0
Anton Bernatskiy <abernats@uvm.edu>
2014-11-19 20:38:21 -0500

    Revert "Amidst the work for connection cost treatment"
    This reverts commit 18566111e3359a743d107df0e048d40dfab01ceb.
    Decided to implement the fitness function transformation in a
    different way.

--- communicators/baseCommunicator.py
+++ communicators/baseCommunicator.py
@@ -4,9 +4,6 @@ class BaseCommunicator(object):
 	def __init__(self):
 		self.cache = {}
 
-	def fitnessTransform(self, fitness, individual):
-		return fitness
-
 	def evaluate(self, indivList):
 		if indivList == []:
 			return indivList
@@ -34,5 +31,5 @@ class BaseCommunicator(object):
 			print str(len(indivList))  + ' != '  + str(len(evaluations))
 			raise IOError('No of evaluations is different from no of individuals')
 		for i in xrange(len(indivList)):
-			indivList[i].setEvaluation(self.fitnessTransform(evaluations[i], indivList[i]))
+			indivList[i].setEvaluation(evaluations[i])
 		return indivList
