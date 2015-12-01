abc4cf31d23f9c105515ed7928d23cebaeb12425
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2015-12-01 14:15:50 -0500

    Automatic backup file detection for the continuation script
    	modified:   continue.py

--- continue.py
+++ continue.py
@@ -1,15 +1,43 @@
 #!/usr/bin/python2
 
 import sys
+import os
 import pickle
 
-if len(sys.argv) == 2:
-	file = open(sys.argv[1], 'r')
-else:
-	raise ValueError('Specify backup filename')
+def loadLastPickle():
+	bcfolder = 'backups'
+	if not os.path.isdir(bcfolder):
+		raise OSError(bcfolder + ' directory not found, cannot continue')
+	files = os.listdir(bcfolder)
+	files = filter(lambda x: x.endswith('.p'), files)
+	files.sort()
+	files.reverse()
+	evolver = None
+	for curFileName in files:
+		fullFileName = os.path.join('backups', curFileName)
+		with open(fullFileName, 'r') as curFile:
+			try:
+				print 'Trying to load ' + fullFileName + ' ...',
+				evolver = pickle.load(curFile)
+				print 'success!'
+				break
+			except:
+				print 'failure'
+				continue
+	if evolver is None:
+		raise OSError('Could not find any suitable pickles at ' + bcfolder + ', cannot continue')
+	else:
+		return evolver
 
-evolver = pickle.load(file)
-file.close()
+if len(sys.argv) == 1:
+	evolver = loadLastPickle()
+elif len(sys.argv) == 2:
+	with open(sys.argv[1], 'r') as file:
+		evolver = pickle.load(file)
+else:
+	print('Usage: evsContinue.py [<backupPickleFileName>]\n'
+				'If no backup file is given, the program automatically searches for the one with a highest number at backups/')
+	sys.exit(1)
 
 evolver.recover()
 evolver.generateLogsAndStdout()
@@ -18,3 +46,5 @@ while True:
 	evolver.updatePopulation()
 	evolver.pickleSelf()
 	evolver.generateLogsAndStdout()
+import pickle
+
