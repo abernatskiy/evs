6eae1bae0f58cd13613519737e5e329888da64aa
Anton Bernatskiy (ishigaki) <abernats@uvm.edu>
2016-02-27 19:46:48 -0500

    CluneSimplified now does not reevaluate the Pareto front elite
    	modified:   evolvers/cluneSimplified.py

--- evolvers/cluneSimplified.py
+++ evolvers/cluneSimplified.py
@@ -57,22 +57,15 @@ class Evolver(BaseEvolver):
 		if r > 0.75:
 			print 'WARNING! Proportion of nondominated individuals too high (' + str(r) + ')'
 
-#		print 'Creating a new population'
 		newPopulation = []
-		for indiv in paretoFront:
-			newPopulation.append(indiv)
+#		for indiv in paretoFront:
+#			newPopulation.append(indiv)
 		while len(newPopulation) < self.params['populationSize']:
-#			print 'Adding a child'
 			parent = np.random.choice(paretoFront)
-#			print 'Chose a parent ' + str(parent)
 			child = deepcopy(parent)
-#			print 'Copying successful'
 			child.mutate()
-#			print 'Mutating a child'
 			newPopulation.append(child)
-#			print 'Appended the child to the new population'
-#		print 'Made a population'
-		self.population = newPopulation
-		self.communicator.evaluate(self.population)
+		self.communicator.evaluate(newPopulation)
+		self.population = paretoFront + newPopulation
+#		self.communicator.evaluate(self.population)
 		self.population.sort(key = lambda x: x.score)
-#		print 'Exiting updatePopulation()'
