2ea20618e0e56cb91737c5deda027acb924a62d8
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2017-01-19 00:05:23 -0500

    In the middle of writing the composite individual
    	modified:   individuals/baseIndividual.py
    	modified:   individuals/compositeFixedProbabilities.py

--- individuals/compositeFixedProbabilities.py
+++ individuals/compositeFixedProbabilities.py
@@ -1,3 +1,6 @@
+import re
+import importlib
+
 from baseIndividual import BaseIndividual
 
                                                                                 
@@ -26,12 +29,43 @@ class Individual(BaseIndividual):
 	     -> "CommonID GEN0 GEN1 ... GENN"
 	'''
 	def __init__(self, params):
-		pass
-	def _processParams(self, params):
-		
+		self._extractPartClasses(params) # must be called before the parameters translator dictionaries are generated
+		self._makeSampleParts()
+#		super(Individual, self).__init__(params) # this calls the translator generators and translates the params
+
+	def _extractPartClasses(self, params):
+		# Extracting names and numbers
+		partClassesNames = {}
+		classPattern = re.compile('^compositeClass[0-9]+$')
+		numPattern = re.compile('[0-9]+$')
+		for pn, pv in params.iteritems():
+			if classPattern.match(pn):
+				classNum = int(numPattern.search(pn).group())
+				partClassesNames[classNum] = pv
+		# Checking if numbers are sequential
+		if partClassesNames.keys() != list(range(len(partClassesNames))):
+			raise ValueError('Classes must be numbered sequentially starting from zero in the config, and tey are not. Exiting.')
+		# Loading the sources for all classes
+		self.partClasses = []
+		for i in range(len(partClassesNames)):
+			self.partClasses.append(importlib.import_module('individuals.' + partClassesNames[i]).Individual) # CAREFUL, might not be in path
+
+	def _makeSampleParts(self):
+		self._sampleParts = []
+		for pcl in self.partClasses:
+			self._sampleParts.append(pcl.initEmpty())
+
+	def requiredParametersTranslator(self):
+		t = super(Individual, self).requiredParametersTranslator()
+		return t
+
+	def optionalParametersTranslator(self):
+		t = super(Individual, self).optionalParametersTranslator()
+		return t
 
 	def __str__(self):
-		# super() has to be redefined
-		pass
+		# Debugging version
+		return 'Individuals within the compound:\n'
+
 	def mutate(self):
 		pass
