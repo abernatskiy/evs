9a9a8639eac1433eab0f4749f2d6f3e0d2833b0a
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2017-04-29 19:58:59 -0400

    Renames
    	renamed:    evolvers/cluneSimplifiedPlusCheney.py -> evolvers/cluneSimplifiedCheney3d.py
    	renamed:    evolvers/cluneSimplifiedEvolutionRateAnnealing.py -> evolvers/cluneSimplifiedProtectedMorphology.py

--- evolvers/cluneSimplifiedCheney3d.py
+++ evolvers/cluneSimplifiedCheney3d.py
@@ -10,6 +10,15 @@ class Evolver(CluneSimplifiedEvolver):
 	def getMorphologicalAge(self):
 		return lambda x: __builtin__.globalGenerationCounter - x.timeOfLastMorphologicalMutation
 
+	def getMinimizableFuncs(self):
+		return ([ self.getErrorFunc(), self.getConnectionCostFunc(), self.getMorphologicalAge() ], ['error', 'connection cost', 'morphological age'])
+
 	def getCluneParetoFront(self):
-		funcs = [ self.getErrorFunc(), self.getConnectionCostFunc(), self.getMorphologicalAge() ]
+		funcs, _ = self.getMinimizableFuncs()
 		return self.findParetoFrontManyObjectives(funcs)
+
+	def doParetoOutput(self, paretoFront):
+		funcs, labels = self.getMinimizableFuncs()
+		self.printParetoFrontMultipleObjectives(paretoFront, funcs, labels)
+		self.logParetoFront(paretoFront)
+		self.paretoWarning(paretoFront)
